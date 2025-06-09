8510f216158623749e09cbfc991d3b5314438b59
Joaquín Paz <joaquin.paz@teladochealth.com>
2025-06-09 17:15:02 -0300

    remove terminal mapping to prefer default kickstart mapping

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -4,7 +4,6 @@ local opts = { noremap = true, silent = true }
 -- Center cursor when scrolling up or down in page
 map('n', '<C-d>', '<C-d>zz', opts)
 map('n', '<C-u>', '<C-u>zz', opts)
-map('t', '<C-Esc>', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
 map({ 'n', 't', 'v', 'i' }, '<C-F4>', ':bd<CR>', { desc = 'Close current buffer' })
 
 -- Terminal configuration
