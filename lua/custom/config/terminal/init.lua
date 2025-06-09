af632022d7689b67ef27d2b2c4674aebb30a3bd3
Joaquín Paz <joaquin.paz@teladochealth.com>
2025-06-09 15:57:13 -0300

    Ignoring terminal configs

--- lua/custom/config/terminal/init.lua
+++ lua/custom/config/terminal/init.lua
@@ -4,7 +4,7 @@ vim.opt.shellquote = ''
 vim.opt.shellxquote = ''
 
 -- Windows 11 config
-vim.opt.shell = '"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"'
+-- vim.opt.shell = '"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"'
 
 -- Windows 10 config
--- vim.opt.shell = '"C:\\Program Files\\Git\\bin\\bash.exe"'
+vim.opt.shell = '"C:\\Program Files\\Git\\bin\\bash.exe"'
