bbb851f3ce1e951f2fa5a79985193a56d2621cf1
Joaquín Paz <joaquin.paz@teladochealth.com>
2025-06-10 17:04:33 -0300

    Configuring fugitive to mange git

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -36,4 +36,11 @@ return {
       },
     },
   },
+  {
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'G', 'Gdiffsplit', 'Gvdiffsplit', 'Gedit' }, -- lazy load
+    keys = {
+      { '<leader>gs', '<cmd>Git<CR>', desc = 'Git status (Fugitive)' },
+    },
+  },
 }
