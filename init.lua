f342fbf9ac17e1c158540f989ee3add4882b0e4c
joacopaz <joacopaz@gmail.com>
2026-04-30 22:34:59 -0300

    Update nvim config

--- init.lua
+++ init.lua
@@ -6,6 +6,12 @@ local USE_TRANSPARENT_BACKGROUND = false
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.filetype.add {
+  extension = {
+    tpl = 'bash',
+  },
+}
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
